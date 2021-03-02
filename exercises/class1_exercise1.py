2b97b457e9985c27faa18747f49985383955674c
LAPTOP-TDG357LH\74049 <740490659@qq.com>
2021-03-02 17:09:09 +0800

    第一节课随堂练习

---
+++ exercises/class1_exercise1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:class1_exercise1.py
+# author:74049
+# datetime:2021/3/2 16:51
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+
+print("helloworld")
+
+a = 10
+b = 2.305
+c = "name"
+print(a)
+print(b)
+print(c)
+
+a = 3
+print(id(a))
+a = 5
+print(id(a))
+
+name=input("输入姓名:")
+print(name)
+
+a,b=input("输入姓名:").split(',')
+print(a,b)
---
+++ exercises/class1_exercise2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:class1_exercise2.py
+# author:74049
+# datetime:2021/3/2 16:53
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+
+import datetime
+import keyword
+if __name__ == '__main__':
+    print("当前时间为:",datetime.datetime.now())
+    print("关键字列表:",keyword.kwlist)
