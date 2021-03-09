83555b17b2f009398be5f13f5bc6307a2c9aedf1
LAPTOP-TDG357LH\74049 <740490659@qq.com>
2021-03-09 16:59:36 +0800

    第三次课堂练习（第三部分）

---
+++ exercises/class3/class3_exercise4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:class3_exercise4.py
+# author:74049
+# datetime:2021/3/9 16:42
+# software: PyCharm
+'''
+this is functiondescription
+'''
+# import module your need
+import random
+
+
+tup = (65, 95, 85, 84, 76, 94, 64, 71, 91, 65)
+
+print('全部分数:', end='')
+for i in tup:
+    print(i, end=' ')
+print('\n', end='')
+print('全班最高分:', max(tup))
